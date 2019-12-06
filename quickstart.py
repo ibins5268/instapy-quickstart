16948f5041490e4187b46615d7039b4e0b8065f8
ibins5268 <58602496+ibins5268@users.noreply.github.com>
2019-12-06 17:38:13 +0100

    Tcf.438
    I love u

--- quickstart.py
+++ quickstart.py
@@ -3,35 +3,35 @@ from instapy import InstaPy
 from instapy import smart_run
 
 # login credentials
-insta_username = ''
-insta_password = ''
+insta_username = tcf.438
+insta_password = Sevilla1+
 
 comments = ['Nice shot! @{}',
-        'I love your profile! @{}',
+        'I love your profile! @{tcf.438}',
         'Your feed is an inspiration :thumbsup:',
         'Just incredible :open_mouth:',
-        'What camera did you use @{}?',
-        'Love your posts @{}',
-        'Looks awesome @{}',
-        'Getting inspired by you @{}',
+        'What camera did you use @{tcf.428}?',
+        'Love your posts @{tcf.438}',
+        'Looks awesome @{tcf.438}',
+        'Getting inspired by you @{tcf.438}',
         ':raised_hands: Yes!',
-        'I can feel your passion @{} :muscle:']
+        'I can feel your passion @{tcf.438} :muscle:']
 
 # get an InstaPy session!
 # set headless_browser=True to run InstaPy in the background
-session = InstaPy(username=insta_username,
-                  password=insta_password,
-                  headless_browser=False)
+session = InstaPy(username=tcf. 438,
+                  password=Sevilla1+,
+                  headless_browser=true)
 
 with smart_run(session):
   """ Activity flow """		
   # general settings		
-  session.set_dont_include(["friend1", "friend2", "friend3"])		
+  session.set_dont_include(["hippo_max", "friend2", "friend3"])		
   
   # activity		
-  session.like_by_tags(["natgeo"], amount=10)
+  session.like_by_tags(["natgeo"], amount=30)
 
   # Joining Engagement Pods
-  session.set_do_comment(enabled=True, percentage=35)
+  session.set_do_comment(enabled=True, percentage=30)
   session.set_comments(comments)
-  session.join_pods(topic='sports', engagement_mode='no_comments')
+  session.join_pods(topic='sports', engagement_mode='comments')
